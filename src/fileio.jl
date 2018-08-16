432b65a6281bd1f0afaa1a45447e2140acb36175
Alex Kunin <abk170@psu.edu>
2018-08-16 10:29:19 -0400

    allow file renaming when saving

--- src/fileio.jl
+++ src/fileio.jl
@@ -19,10 +19,11 @@ end
 #### File IO
 ################################################################################
 """
-    savespikes(S::SpikeTrains; [filename], [dir])
+    savespikes(S::SpikeTrains; [filename], [dir], [old])
 
 Save the given spike trains to disk using the `JLD` package. Returns the full path to the
-saved file.
+saved file. Deletes file `old` if it exists in `dir`. By default `old` is `nothing` to avoid
+unnecessarily deleting and rewriting files.
 
 Default `filename` is `string(hash(S))`. Default `dir` is `Pkg.dir(Spikes)/saved`.
 
@@ -30,11 +31,18 @@ If `dir` doesn't exist, will use `mkpath`. If file exists, contents will be over
 """
 function savespikes(S::SpikeTrains;
     filename=string(hash(S)),
-    dir=joinpath(Pkg.dir("Spikes"), "saved"))
+    dir=joinpath(Pkg.dir("Spikes"), "saved"),
+    old=nothing)
 
     # _dir = abspath(dir)
     if !ispath(dir)
         mkpath(dir)
+    end
+    if old != nothing
+        _old = endswith(old, ".jld") ? old : old * ".jld"
+        if isfile(joinpath(dir, _old))
+            rm(joinpath(dir, _old))
+        end
     end
     _fn = endswith(filename, ".jld") ? filename : filename * ".jld"
     # save(joinpath(_dir, _fn), "S", S)
